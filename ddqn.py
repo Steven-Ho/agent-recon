b3bd43d8589c00e04dd0604b0c613f3cabd6211f
Steven Ho <steven.yyyz@live.cn>
2020-08-25 21:43:59 +0800

    env loop

--- ddqn.py
+++ ddqn.py
@@ -6,6 +6,32 @@ class DDQN:
         self.args = args
         self.obs_shape = obs_shape
         self.action_shape = action_shape
-        self.layers = [
-            tf.keras.layers.Conv2D()
-        ]
+        conv1 = tf.keras.layers.Conv2D(16, 11, strides=(3, 3), input_shape=obs_shape)
+        pool1 = tf.keras.layers.MaxPooling2D()
+        # pool1 output: 33, 25, 16
+        conv2 = tf.keras.layers.Conv2D(32, 5, strides=(1, 1), input_shape=(33, 25, 16))
+        pool2 = tf.keras.layers.MaxPooling2D()
+        # pool2 output: 14, 10, 32
+        flat = tf.keras.layers.Flatten()
+        fc1 = tf.keras.layers.Dense(128, activation='relu')
+        fc2 = tf.keras.layers.Dense(action_shape)
+        self.q1 = tf.keras.Sequential([conv1, pool1, conv2, pool2, flat, fc1, fc2])
+        self.q2 = tf.keras.models.clone_model(self.q1)
+        self.optimizer = tf.keras.optimizers.Adam(0.001)
+        self.loss = tf.keras.losses.MeanSquaredError()
+        print("Initilized!")
+
+    @tf.function
+    def forward(self, obs):
+        q_1 = self.q1(obs, training=False)
+        q_2 = self.q2(obs, training=False)
+        return q_1, q_2
+
+    @tf.function
+    def train(self, obs, target):
+        with tf.GradientTape() as tape:
+            q_1 = self.q1(obs, training=True)
+            q_loss_1 = self.loss(target, q_1)
+
+        gradients = tape.gradient(q_loss_1, self.q1.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.q1.trainable_variables))
--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import gym
 import numpy as np 
 import tensorflow as tf 
 import argparse
+from ddqn import DDQN
+import itertools
+import time
 
 parser = argparse.ArgumentParser(description='Duoble DQN Baseline')
 parser.add_argument('--scenario', type=str, default="Seaquest-v0", help="environment")
 parser.add_argument('--seed', type=int, default=123, help="random seed for env")
+parser.add_argument('--num_episodes', type=int, default=40000, help='number of episodes for training')
+parser.add_argument('--max_episode_len', type=int, default=100, help='maximum episode length')
+parser.add_argument('--gamma', type=float, default=0.95, help='discount factor (default: 0.99)')
 
 args = parser.parse_args()
 
@@ -13,6 +19,19 @@ env = gym.make(args.scenario)
 env.seed(args.seed)
 np.random.seed(args.seed)
 
-obs_shape_list = env.observation_space.obs_shape_list
+obs_shape_list = env.observation_space.shape
 action_shape = env.action_space.n
+
+qnet = DDQN(obs_shape_list, action_shape, args)
+total_numsteps = 0
+updates = 0
+t_start = time.time()
+for i_episode in itertools.count(1):
+        
+    state = env.reset()
+    state = np.expand_dims(state, 0)/255.
+
+q1, q2 = qnet.forward(state)
+q1 = q1.numpy().squeeze()
+q2 = q2.numpy().squeeze()
 print("End of script.")
