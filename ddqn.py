feb7b240e99a460d8390eaab0c7e1d94e969ffeb
Steven Ho <steven.yyyz@live.cn>
2020-08-27 17:02:57 +0800

    debug version of tf with gpu

--- ddqn.py
+++ ddqn.py
@@ -9,13 +9,17 @@ class DDQN:
         conv1 = tf.keras.layers.Conv2D(16, 11, strides=(3, 3), input_shape=obs_shape)
         pool1 = tf.keras.layers.MaxPooling2D()
         # pool1 output: 33, 25, 16
-        conv2 = tf.keras.layers.Conv2D(32, 5, strides=(1, 1), input_shape=(33, 25, 16))
+        conv2 = tf.keras.layers.Conv2D(32, 5, strides=(1, 1))
         pool2 = tf.keras.layers.MaxPooling2D()
         # pool2 output: 14, 10, 32
+        conv3 = tf.keras.layers.Conv2D(32, 3, strides=(1, 1))
+        pool3 = tf.keras.layers.MaxPooling2D()
+        # pool3 output: 14, 10, 32
         flat = tf.keras.layers.Flatten()
         fc1 = tf.keras.layers.Dense(128, activation='relu')
         fc2 = tf.keras.layers.Dense(action_shape)
-        self.q1 = tf.keras.Sequential([conv1, pool1, conv2, pool2, flat, fc1, fc2])
+        self.q1 = tf.keras.Sequential([conv1, pool1, conv2, pool2, conv3, pool3, flat, fc1, fc2])
+        # self.q1 = tf.keras.Sequential([flat, fc1, fc2])
         self.q2 = tf.keras.models.clone_model(self.q1)
         self.optimizer = tf.keras.optimizers.Adam(0.001)
         self.loss = tf.keras.losses.MeanSquaredError()
@@ -81,4 +85,4 @@ class DDQN:
         gradients = tape2.gradient(q_loss_2, self.q2.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.q2.trainable_variables))
 
-        return q_loss_1, q_loss_2
+        return q_loss_1.numpy().tolist(), q_loss_2.numpy().tolist()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ddqn import DDQN
 from buffer import FullReplayMemory
 import itertools
 import time
-
+# tf.debugging.set_log_device_placement(True)
 parser = argparse.ArgumentParser(description='Duoble DQN Baseline')
 parser.add_argument('--scenario', type=str, default="Seaquest-v0", help="environment")
 parser.add_argument('--seed', type=int, default=123, help="random seed for env")
@@ -15,7 +15,7 @@ parser.add_argument('--max_episode_len', type=int, default=100, help='maximum ep
 parser.add_argument('--gamma', type=float, default=0.95, help='discount factor (default: 0.95)')
 parser.add_argument('--epsilon', type=float, default=0.1, help='epsilon-greedy parameter (initial: 0.1)')
 parser.add_argument('--buffer_size', type=int, default=1e6, help='maximum size for replay buffer')
-parser.add_argument('--update_interval', type=int, default=1, help='update q network for every N steps')
+parser.add_argument('--update_interval', type=int, default=10, help='update q network for every N steps')
 parser.add_argument('--startup_steps', type=int, default=1000, help='initial rollout steps before training')
 parser.add_argument('--batch_size', type=int, default=128, help='sample size for training')
 args = parser.parse_args()
@@ -29,33 +29,42 @@ action_shape = env.action_space.n
 
 qnet = DDQN(obs_shape_list, action_shape, args)
 memory = FullReplayMemory(args.buffer_size)
+writer = tf.summary.create_file_writer("logs/{}".format(args.scenario))
 
 total_numsteps = 0
 timestep = 0
 t_start = time.time()
-for i_episode in itertools.count(1):
-        
-    obs = env.reset()
-    obs = obs/255.
-    done = False
 
-    episode_reward = 0
-    for t in range(args.max_episode_len):
-        timestep += 1
-        action = qnet.act(np.expand_dims(obs, 0))
-        new_obs, reward, done, _ = env.step(action)
-        new_obs = new_obs/255.
+total_parameters = np.sum([np.prod(v.get_shape().as_list()) for v in qnet.q1.trainable_variables])
+with writer.as_default():
+    for i_episode in itertools.count(1):
+            
+        obs = env.reset()
+        obs = obs/255.
+        done = False
 
-        memory.push((obs, action, reward, done, new_obs))
-        obs = new_obs
+        episode_reward = 0
+        for t in range(args.max_episode_len):
+            timestep += 1
+            action = qnet.act(np.expand_dims(obs, 0))
+            new_obs, reward, done, _ = env.step(action)
+            new_obs = new_obs/255.
 
-        if timestep > args.startup_steps:
-            if timestep % args.update_interval == 0:
-                obs, action, reward, done, new_obs = memory.sample(args.batch_size)
-                qnet.update((obs, action, reward, done, new_obs))
+            memory.push((obs, action, reward, done, new_obs))
+            obs = new_obs
 
-        episode_reward += reward
-        if done:
-            break
+            if timestep > args.startup_steps:
+                if timestep % args.update_interval == 0:
+                    obs_b, action_b, reward_b, done_b, new_obs_b = memory.sample(args.batch_size)
+                    lq1, lq2 = qnet.update((obs_b, action_b, reward_b, done_b, new_obs_b))
+                    tf.summary.scalar("loss/q1", lq1, step=timestep)
+                    tf.summary.scalar("loss/q2", lq2, step=timestep)
+                    writer.flush()
+
+            episode_reward += reward
+            if done:
+                break
+        print("Episode reward: {}".format(episode_reward))
+        episode_reward = 0
 
 print("End of script.")
